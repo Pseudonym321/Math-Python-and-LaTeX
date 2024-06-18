fe7e5be744a131ccfd2b4eebceb9e3d8a8e34d68
Jasper <143286960+Pseudonym321@users.noreply.github.com>
2024-06-18 15:50:06 -0600

    Mathematics

--- Programming/Python/Mathematics/Modules/riemann_integral.py
+++ Programming/Python/Mathematics/Modules/riemann_integral.py
@@ -1,25 +1,17 @@
-preamble_beginning = r"""\documentclass{beamer}
-\beamertemplatenavigationsymbolsempty
-\usepackage{mathtools,amsmath,amssymb,amsfonts,physics}
-\usepackage{tikz,scalerel,tikz-3dplot,tkz-euclide}
-\usepackage{pict2e,ifthen}
-\usetikzlibrary{calc,patterns,arrows.meta}
-\usetikzlibrary{shadows,external,perspective,spath3}
-\usetikzlibrary{decorations.pathreplacing,angles,quotes}
-"""
+def Riemann_sum(initial_bound:float, terminal_bound:float, number_of_denominations:int):
+    """
+    Purpose:
+        Given two bounds and a number of denominations, returns the width of the individual denominations.
+    Parameters:
+        float - initial_bound: 
+        float - terminal_bound:
+        int - number_of_denominations:
+    Return:
+        float - denomination_width:
+        list[float] - left_Riemann_inputs
+    """
+    denomination_width = (terminal_bound - initial_bound)/number_of_denominations # - float
+    left_Riemann_inputs = [(initial_bound + denomination_number * denomination_width) for denomination_number in range(number_of_denominations)]
+    return denomination_width, left_Riemann_inputs
 
-postscript = r"""
-line 7
-line 8
-line 9"""
-
-with open("TeX_file.tex", "a+") as tex:
-    tex.write(preamble_beginning)
-    for line_number in range(4,7):
-        if line_number != 6:
-            tex.write(f"line {line_number}\n")
-        else:
-            tex.write(f"line {line_number}")
-    tex.write(postscript)
-    tex.seek(0)
-    print(tex.read())
+print(Riemann_sum(-5,6,10)[1])
