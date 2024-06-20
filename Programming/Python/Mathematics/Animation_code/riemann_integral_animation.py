0c9d1acb6f629373452ed86d595e1119475af36a
Pseudonym321 <twilliam1925@outlook.com>
2024-06-19 21:50:42 -0600

    Mathematics

--- Programming/Python/Mathematics/Animation_code/riemann_integral_animation.py
+++ Programming/Python/Mathematics/Animation_code/riemann_integral_animation.py
@@ -1,5 +1,6 @@
 import Riemann_integral as Ri
-import subprocess, shutil
+import subprocess
+import shutil
 import os
 from pypdf import PdfReader, PdfWriter
 preamble = r"""\documentclass{beamer}
@@ -10,7 +11,7 @@ preamble = r"""\documentclass{beamer}
 \centering
 \begin{tikzpicture}[scale=3]
 \draw[thick,-latex] (-1.3,0) -- (1.3,0);
-\draw[thick,-latex] (0,-0.3) -- (0,2.3);
+%\draw[thick,-latex] (0,-0.3) -- (0,2.3);
 \draw[domain=-1.23:1.18,smooth,variable=\Vt]
   plot (\Vt,{\Vt*\Vt*\Vt-0.5*\Vt+1},0);
 \draw[] (-1,0) -- (-1,2) node[pos=1,above]{$a$};
@@ -33,21 +34,32 @@ def remove_first_page(input_pdf, output_pdf):
     with open(output_pdf, 'wb') as output_file:
         pdf_writer.write(output_file)
 
-merged_pdf = os.path.join("", 'merged_output.pdf')
 
+# Function to compile a TeX file to PDF
+def compile_tex_to_pdf(tex_file):
+    subprocess.run(['pdflatex', tex_file])
 
+# Function to append two PDF files
+def append_pdfs(pdf1, pdf2, output_pdf):
+    subprocess.run(['gswin64c', '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + output_pdf, os.path.abspath(pdf1), os.path.abspath(pdf2)])
+    #subprocess.run(['mgs', '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + output_pdf, pdf1, pdf2])
 
+tex_file = "TeX_file.tex"
+pdf_file = "TeX_file.pdf"
+output_directory = r''
+merged_pdf = os.path.join(output_directory, 'merged_output.pdf')
 for num in range (5,30):
-    with open("TeX_file.tex", "w") as tex:
+    with open(tex_file, "w") as tex:
         tex.write(preamble)
         delta, inputs = Ri.Riemann_sum(-1,1,num)
         for input in inputs:
+            new_line = f"\\fill[blue, opacity=0.3] ({input},0) -- ({input},{{{input}^3-0.5*{input}+1}}) -- ++({delta},0) |- (0,0);\n"
+            tex.write(new_line)
             new_line = f"\\draw[] ({input},0) -- ({input},{{{input}^3-0.5*{input}+1}}) -- ++({delta},0) |- (0,0);\n"
             tex.write(new_line)
         tex.write(postscript)
-    subprocess.run(['pdflatex', 'TeX_file.tex'])
-    temp_pdf = os.path.join("", 'merged_output_temp.pdf')
-    subprocess.run(['gswin64c', '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + "TeX_file.pdf", os.path.abspath(merged_pdf), os.path.abspath(temp_pdf)])
+    compile_tex_to_pdf(tex_file)
+    temp_pdf = os.path.join(output_directory, 'merged_output_temp.pdf')
+    append_pdfs(merged_pdf, pdf_file, temp_pdf)
     shutil.move(temp_pdf, merged_pdf)
-remove_first_page(merged_pdf, os.path.join(merged_pdf, 'final_output.pdf'))
-    
+remove_first_page(merged_pdf, os.path.join(output_directory, 'final_output.pdf'))
