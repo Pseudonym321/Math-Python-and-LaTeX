982d9282bceb988456dc22d383187ff7da958784
Tyler Jaglal <twilliam1925@outlook.com>
2024-06-25 16:23:52 -0600

    planets2

--- Python/Animation_code/revolving_around_sun_and_spinning.py
+++ Python/Animation_code/revolving_around_sun_and_spinning.py
@@ -26,26 +26,42 @@ postscript = r"""
 
 \draw[dashed] (0,0) circle(6);
 
-\foreach \Vthe in {0, 10, ..., 170}{
-%%% SPHERE 1 %%%
-\tdplotsetrotatedcoords{\Vazi}{\Vpol}{0}
-%%% LONGITUDE %%%
-\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({cos(\t)*cos(\Vthe+\Vspin)},{cos(\t)*sin(\Vthe+\Vspin)},{sin(\t)});
-\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)},{3*sin(\Vang)},{0})}}}];
-%%% LATITUDE %%%
-\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({cos(\t)*sin(\Vthe)},{sin(\t)*sin(\Vthe)},{cos(\Vthe)});
-\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)},{3*sin(\Vang)},{0})}}}];
 
-%%% SPHERE 2 %%%
+\foreach \Vthe in {0, 10, ..., 170}{
+%%% MAIN SPHERE %%%
 \tdplotsetrotatedcoords{0}{0}{0}
 %%% LONGITUDE %%%
-\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({1.5*cos(\t)*cos(\Vthe+\Vspin)},{1.5*cos(\t)*sin(\Vthe+\Vspin)},{1.5*sin(\t)});
+\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({1.5*cos(\t)*cos(\Vthe)},{1.5*cos(\t)*sin(\Vthe)},{1.5*sin(\t)});
 \path[draw,black,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];
 %%% LATITUDE %%%
 \path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({1.5*cos(\t)*sin(\Vthe)},{1.5*sin(\t)*sin(\Vthe)},{1.5*cos(\Vthe)});
 \path[draw,black,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];}
 
-\draw[white,tdplot_screen_coords] (0,0) circle(8);
+\foreach \Vthe in {0, 20, ..., 160}{
+%%% SPHERE 1 %%%
+\tdplotsetrotatedcoords{\Vazi}{\Vpol}{0}
+%%% LONGITUDE %%%
+\path[dashed,spath/save=pathname] ({0},{0},0) circle(3);
+\path[draw,black,very thin,dashed,spath/use={pathname, transform={shift={({3*cos(\Vang)},{3*sin(\Vang)},{0})}}}];
+\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({cos(\t)*cos(\Vthe+\Vspin)},{cos(\t)*sin(\Vthe+\Vspin)},{sin(\t)});
+\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)},{3*sin(\Vang)},{0})}}}];
+%%% LATITUDE %%%
+\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({cos(\t)*sin(\Vthe)},{sin(\t)*sin(\Vthe)},{cos(\Vthe)});
+\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)},{3*sin(\Vang)},{0})}}}];}
+
+
+\foreach \Vthe in {0, 20, ..., 160}{
+%%% SPHERE 2 %%%
+\tdplotsetrotatedcoords{\Vazi}{-\Vpol}{0}
+%%% LONGITUDE %%%
+\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({0.45*cos(\t)*cos(\Vthe+\Vspin)},{0.45*cos(\t)*sin(\Vthe+\Vspin)},{0.45*sin(\t)});
+\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)+1.5*cos(\Vangle)},{3*sin(\Vang)+1.5*sin(\Vangle)},{0})}}}];
+%%% LATITUDE %%%
+\path[tdplot_rotated_coords,very thin,domain=0:360,smooth,variable=\t,spath/save=pathname] plot ({0.45*cos(\t)*sin(\Vthe)},{0.45*sin(\t)*sin(\Vthe)},{0.45*cos(\Vthe)});
+\path[draw,black,very thin,spath/use={pathname, transform={shift={({3*cos(\Vang)+1.5*cos(\Vangle)},{3*sin(\Vang)+1.5*sin(\Vangle)},{0})}}}];}
+
+
+\draw[white,tdplot_screen_coords] (0,0) circle(10);
 
 \end{tikzpicture}
 \end{frame}
@@ -82,9 +98,11 @@ merged_pdf = os.path.join(output_directory, 'merged_output.pdf')
 for angle in range(0,360,360//24):
     with open(tex_file, "w") as tex:
         tex.write(preamble)
-        new_line = f"\\newcommand{{\\Vspin}}{{{angle*3//2}}}\n"
+        new_line = f"\\newcommand{{\\Vspin}}{{{angle*2}}}\n"
         tex.write(new_line)
         new_line = f"\\newcommand{{\\Vang}}{{{angle}}}\n"
+        tex.write(new_line)
+        new_line = f"\\newcommand{{\\Vangle}}{{{angle*2}}}\n"
         tex.write(new_line)
         tex.write(postscript)
     compile_tex_to_pdf(tex_file)
