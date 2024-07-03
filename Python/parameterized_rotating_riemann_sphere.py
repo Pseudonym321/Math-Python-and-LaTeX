7ff6a8528ca4b6582f93c0a7dc68d3811fd9b4e2
Jasper Nice <twilliam1925@outlook.com>
2024-07-03 08:59:41 -0600

    rotating_parallel_riemann_sphere

--- Python/parameterized_rotating_riemann_sphere.py
+++ Python/parameterized_rotating_riemann_sphere.py
@@ -1,14 +1,9 @@
-# %%
-import numpy as np
-import os
-import shutil
-from PyPDF2 import PdfReader, PdfWriter
-import subprocess
 
-# %%
+import numpy as np
+import Modules.animatetex as animatetex
+
 numiter = 24
 
-# %%
 start= r'''
 \documentclass{beamer}
 \beamertemplatenavigationsymbolsempty
@@ -22,7 +17,6 @@ start= r'''
 \begin{document}
 '''
 
-# %%
 end = r'''
 \begin{frame}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -35,7 +29,6 @@ end = r'''
 \tdplotsetmaincoords{\polar}{\azimuth}
 \begin{tikzpicture}[tdplot_main_coords, scale=\myscale]
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% COORDINATE AXES AND GRID %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -46,8 +39,6 @@ end = r'''
 \draw[thin,dotted] (\x,-3.75) -- (\x,3.75);}
 \foreach \y in {-3.75,-3.5,...,3.75}{
 \draw[thin,dotted] (-3.75,\y) -- (3.75,\y);}
-
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% BLANK OUT PARTS ON TOP %%%
@@ -92,121 +83,21 @@ end = r'''
 \path[draw,yellow,spath/use={name,transform={shift={({sin(\n)*\myscale*\x/(1+(\x)^2)},{-cos(\n)*\myscale*\x/(1+(\x)^2)},{\myscale*((((\x)^2-1)/((\x)^2+1))+1)/2})}}}];
 }
 
-
-
 \path[draw,white,tdplot_screen_coords] (-5.5,-4.5) rectangle (5.5,4.5); 
 \end{tikzpicture}
-
 \end{frame}
-
 \end{document}
-
 '''
 
-# %%
-def make_tikz_code(angle):
-    tikz_code = r''
-    tikz_code += start
-    tikz_code += r'\def \n {' + f'{angle}' + '}'
-    tikz_code += end
-    return tikz_code
+def main():
+    animatetex.before_loop()
+    for angle in np.linspace(0,60,numiter):
+        with open(animatetex.TeX_file, 'w') as f:
+            f.write(start)
+            f.write(r'\def \n {' + f'{angle}' + '}')
+            f.write(end)
+        animatetex.during_loop()
+    animatetex.after_loop()
 
-# %%
-def remove_first_page(input_pdf, output_pdf):
-    pdf_reader = PdfReader(input_pdf)
-    pdf_writer = PdfWriter()
-
-    # Add all pages except the first one
-    for page_num in range(1, len(pdf_reader.pages)):
-        pdf_writer.add_page(pdf_reader.pages[page_num])
-
-    with open(output_pdf, 'wb') as output_file:
-        pdf_writer.write(output_file)
-
-# %%
-output_directory = r''
-
-# %%
-# Function to compile a TeX file to PDF
-def compile_tex_to_pdf(tex_file):
-    subprocess.run(['pdflatex', tex_file])
-
-# %%
-# Function to append two PDF files
-gs_path = r"C:\Program Files\gs\gs10.03.1\bin\gswin64c.exe"
-gs_path= r"gswin64c"
-def append_pdfs(pdf1, pdf2, output_pdf):
-    subprocess.run([gs_path, '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + output_pdf, os.path.abspath(pdf1), os.path.abspath(pdf2)])
-
-# %%
-# Initial PDF file
-merged_pdf = os.path.join(output_directory, 'merged_output.pdf') # make this the first frame
-
-# %%
-for i in np.linspace(0,30,numiter):
-    # Create a TeX file for each iteration
-    tex_file = 'file_{}.tex'.format(1)
-    tikz_code = make_tikz_code(i)
-    with open(tex_file, 'w') as f:
-        f.write(tikz_code)
-
-    # Compile the TeX file to PDF
-    compile_tex_to_pdf(tex_file)
-
-    # Append the generated PDF to the merged PDF
-    temp_pdf = os.path.join(output_directory, 'merged_output_temp.pdf')
-    append_pdfs(merged_pdf, 'file_{}.pdf'.format(1), temp_pdf)
-
-    # Rename the temporary merged PDF to the original merged PDF
-    shutil.move(temp_pdf, merged_pdf)
-
-
-
-# %%
-print('Merged PDF file:', merged_pdf)
-
-# %%
-remove_first_page(merged_pdf, os.path.join(output_directory, 'final_output.pdf'))
-
-# %%
-# Clean up - remove individual TeX and PDF files
-try:
-    os.remove('file_1.tex')
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_1.pdf')
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.aux'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.log'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.nav'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.out'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.snm'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    os.remove('file_{}.toc'.format(1))
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-try:
-    pass
-    os.remove('merged_output.pdf')
-except FileNotFoundError:
-    pass  # Ignore if the file is not found
-print('Cleaned up temporary files.')
-
-
+if __name__ == "__main__":
+    main()
