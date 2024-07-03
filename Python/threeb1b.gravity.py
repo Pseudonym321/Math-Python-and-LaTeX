9437657839a7ce865413b7d6a8a826df81c2998a
Jasper Nice <twilliam1925@outlook.com>
2024-07-03 09:52:57 -0600

    toroidal_field

--- Python/threeb1b.gravity.py
+++ Python/threeb1b.gravity.py
@@ -1,7 +1,6 @@
 
 import numpy as np
-import os
-import shutil
+import Modules.animatetex as animatetex
 numiter = 24
 start = r'''
 \documentclass{beamer}
@@ -38,85 +37,15 @@ end = r'''
 \end{document}
 '''
 
-def make_tikz_code(angle):
-    string = r''
-    string += start
-    string += r'\newcommand{\p}{' +f'{angle}' +'}'
-    string += end
-    return string
+def main():
+    animatetex.before_loop()
+    for angle in np.linspace(0,90,numiter//2):
+        with open(animatetex.TeX_file, 'w') as f:
+            f.write(start)
+            f.write(r'\newcommand{\p}{' +f'{angle}' +'}')
+            f.write(end)
+        animatetex.during_loop()
+    animatetex.after_loop()
 
-
-
-from pypdf import PdfReader, PdfWriter
-
-def remove_first_page(input_pdf, output_pdf):
-    pdf_reader = PdfReader(input_pdf)
-    pdf_writer = PdfWriter()
-
-    # Add all pages except the first one
-    for page_num in range(1, len(pdf_reader.pages)):
-        pdf_writer.add_page(pdf_reader.pages[page_num])
-
-    with open(output_pdf, 'wb') as output_file:
-        pdf_writer.write(output_file)
-
-
-import subprocess
-
-output_directory = r''
-
-# Function to compile a TeX file to PDF
-def compile_tex_to_pdf(tex_file):
-    subprocess.run(['pdflatex', tex_file])
-
-# Function to append two PDF files
-def append_pdfs(pdf1, pdf2, output_pdf):
-    subprocess.run(['gswin64c', '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + output_pdf, os.path.abspath(pdf1), os.path.abspath(pdf2)])
-    #subprocess.run(['mgs', '-dBATCH', '-dNOPAUSE', '-q', '-sDEVICE=pdfwrite', '-sOutputFile=' + output_pdf, pdf1, pdf2])
-
-
-# Initial PDF file
-merged_pdf = os.path.join(output_directory, 'merged_output.pdf') # make this the first frame
-
-
-
-for i in np.linspace(0,90,numiter//2):
-    # Create a TeX file for each iteration
-    tex_file = 'file_{}.tex'.format(1)
-    string = make_tikz_code(i)
-    with open(tex_file, 'w') as f:
-        f.write(string)
-    
-    # Compile the TeX file to PDF
-    compile_tex_to_pdf(tex_file)
-
-    # Append the generated PDF to the merged PDF
-    temp_pdf = os.path.join(output_directory, 'merged_output_temp.pdf')
-    append_pdfs(merged_pdf, 'file_{}.pdf'.format(1), temp_pdf)
-
-    # Rename the temporary merged PDF to the original merged PDF
-    shutil.move(temp_pdf, merged_pdf)
-    #subprocess.run(['mv', 'merged_output_temp.pdf', merged_pdf])
-
-for i in np.linspace(90,0,numiter//2):
-    # Create a TeX file for each iteration
-    tex_file = 'file_{}.tex'.format(1)
-    string = make_tikz_code(i)
-    with open(tex_file, 'w') as f:
-        f.write(string)
-
-    # Compile the TeX file to PDF
-    compile_tex_to_pdf(tex_file)
-
-    # Append the generated PDF to the merged PDF
-    temp_pdf = os.path.join(output_directory, 'merged_output_temp.pdf')
-    append_pdfs(merged_pdf, 'file_{}.pdf'.format(1), temp_pdf)
-
-    # Rename the temporary merged PDF to the original merged PDF
-    shutil.move(temp_pdf, merged_pdf)
-    #subprocess.run(['mv', 'merged_output_temp.pdf', merged_pdf])
-
-
-
-
-remove_first_page(merged_pdf, os.path.join(output_directory, 'final_output.pdf'))
+if __name__ == "__main__":
+    main()
