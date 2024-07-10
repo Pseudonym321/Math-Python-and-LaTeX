614a0716d1d349bd90c29663ba21bbc37cafc626
Jasper Nice <twilliam1925@outlook.com>
2024-07-10 01:01:51 -0600

    toroidal_field

--- Python/toroidal_field.py
+++ Python/toroidal_field.py
@@ -7,49 +7,60 @@ start= r'''
 \beamertemplatenavigationsymbolsempty
 \usepackage{tikz,tikz-3dplot}
 \usetikzlibrary{spath3}
+%%% COMMANDS %%%
+\newcommand{\Clat}[1]{(1/cos(#1))}
+\newcommand{\Rlat}[1]{(sqrt((1/(cos(#1))^2)-abs(cos(#1))))}
+\newcommand{\Clon}[1]{(-sin(#1)/cos(#1))}
+\newcommand{\Rlon}[1]{(1/cos(#1))}
 '''
 end = r'''
 \begin{document}
-\begin{frame}
 \centering
-\tdplotsetmaincoords{40}{\Vtheta}
+\tdplotsetmaincoords{60}{\Vazi}
 \begin{tikzpicture}[tdplot_main_coords]
-
-%%% DEFINITIONS %%%
-\def \Vazi {0}
-
-%%% OUTTER ELLIPSE %%%
-%\draw[very thin,tdplot_screen_coords] (0,0,0) circle(1);
+%%% AXES %%%
 %\draw[-latex] (-3,0,0) -- (3,0,0) node[pos=1]{$x$};
 %\draw[-latex] (0,-3,0) -- (0,3,0) node[pos=1]{$y$};
 %\draw[-latex] (0,0,-3) -- (0,0,3) node[pos=1]{$z$};
 
 
-%%% FOREACH LOOP %%%
-\foreach \Vthe in {0, 10, ..., 350}{
-%%% LONGITUTE %%%
-\tdplotsetrotatedcoords{\Vazi-90}{\Vthe}{0}
-\path[tdplot_rotated_coords,very thin,spath/save=pathname] (0,0,0) circle(1);
-\path[draw,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];
-%%% LATITUTE %%%
-\tdplotsetrotatedcoords{\Vazi}{90}{0}
-\path[tdplot_rotated_coords,very thin,spath/save=pathname] (0,0,0) circle [radius={cos(\Vthe)}];
-\path[tdplot_rotated_coords,draw,very thin,spath/use={pathname, transform={shift={({0},{0},{sin(\Vthe)})}}}];
-%%% MAGNETIC FIELD %%%
-\foreach \Vdist in {20,40,60}{
-\tdplotsetrotatedcoords{\Vazi-90}{\Vthe}{0}
-\path[tdplot_rotated_coords,very thin,spath/save=pathname] ({1/cos(\Vdist)},0,0) circle({sqrt(1/(cos(\Vdist))^2-cos(\Vdist))});
-\path[draw,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];
-\tdplotsetrotatedcoords{\Vazi-90}{\Vthe}{90}
-\path[tdplot_rotated_coords,very thin,spath/save=pathname] ({-sin(\Vdist)/cos(\Vdist)},0,0) circle({1/cos(\Vdist)});
-\path[draw,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];
-\path[tdplot_rotated_coords,very thin,spath/save=pathname] ({sin(\Vdist)/cos(\Vdist)},0,0) circle({1/cos(\Vdist)});
-\path[draw,very thin,spath/use={pathname, transform={shift={({0},{0},{0})}}}];
+
+\draw[tdplot_screen_coords,white] (-5,-4) rectangle (5,4.5);
+\clip[tdplot_screen_coords] (-5,-4) rectangle (5,4);
+
+
+
+%%% 60 TORUS %%%
+\foreach \Vt in {0,30,60,87,93,120,150,180}{
+%%% LONGITUDE %%%
+\foreach \Vtheta in {0,10,...,350}{
+\tdplotsetrotatedcoords{\Vtheta+90}{0}{0}
+\draw[tdplot_rotated_coords,domain=0:360,smooth,variable=\Vp,very thin] plot (0,{\Rlat{\Vt}*sin(\Vp)+\Clat{\Vt}},{\Rlat{\Vt}*cos(\Vp)});
+}
+%%% LATITUDE %%%
+\foreach \Vtheta in {0,10,...,350}{
+\draw[domain=0:360,smooth,variable=\Vp,very thin] plot ({\Rlon{\Vt}+cos(\Vtheta)*\Rlat{\Vt})*sin(\Vp)},{\Rlon{\Vt}+cos(\Vtheta)*\Rlat{\Vt})*cos(\Vp)},{\Rlat{\Vt}*sin(\Vtheta)});
 }
 
+%\def \Vn {7}
+%\def \Vq {40}
+%\draw[domain=0:360,samples=1000,red,smooth,variable=\Vp] plot ({((1/(cos(\Vt)))+(sqrt((1/((cos(\Vt))^2))-cos(\Vt))*cos(\Vn*\Vp)))*cos(\Vq*\Vp)},{((1/(cos(\Vt)))+(sqrt((1/((cos(\Vt))^2))-cos(\Vt))*cos(\Vn*\Vp)))*sin(\Vq*\Vp)},0{sqrt((1/((cos(\Vt))^2))-cos(\Vt))*sin(\Vn*\Vp)});
+
+%%% 60 SPHERE %%%
+%%% LATITUDE %%%
+\foreach \Vtheta in {0,10,...,350}{
+\tdplotsetrotatedcoords{\Vtheta}{0}{0}
+\draw[tdplot_rotated_coords,variable=\Vp,smooth,domain=0:360,very thin] plot ({\Rlon{\Vt}*cos(\Vp)},{0},{\Rlon{\Vt}*sin(\Vp)+\Clon{\Vt}});
 }
+%%% LONGITUDE %%%
+\foreach \Vtheta in {0,10,...,350}{
+\draw[variable=\Vp,smooth,domain=0:360,very thin] plot ({sin(\Vtheta)*\Rlon{\Vt}*cos(\Vp)},{sin(\Vtheta)*\Rlon{\Vt}*sin(\Vp)},{\Clon{\Vt}+\Rlon{\Vt}*cos(\Vtheta)});
+}
+}
+
 \end{tikzpicture}
-\end{frame}
+
+
 \end{document}
 '''
 
@@ -58,7 +69,7 @@ def main():
     for angle in np.linspace(0,360,numiter):
         with open(animatetex.TeX_file, 'w') as f:
             f.write(start)
-            f.write(r'\newcommand{\Vtheta}{' +f'{angle}' +'}')
+            f.write(r'\newcommand{\Vazi}{' +f'{angle}' +'}')
             f.write(end)
         animatetex.during_loop()
     animatetex.after_loop()
